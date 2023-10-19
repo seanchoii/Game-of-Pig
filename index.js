3a9853b72ee0cdc5058e6236587e6d4efacf4f22
seanchoii <Vertong05@gmail.com>
2023-10-19 13:33:35 -0600

    added roll and hold buttons

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
 const play = document.getElementById('playButton');
+const game = document.getElementById('game');
 
-canvas.width = 1024;
-canvas.height = 574;
 
 play.addEventListener("click", () => {
-    document.getElementById('menu').style.display = "none";
-    c.fillRect(0, 0, canvas.width, canvas.height); 
+    //document.getElementById('menu').style.display = "none";
+    //game.style.display = 'flex';
 })
+
+function diceAnimation(){
+    // dice animation
+}
+
+function diceRandom(){
+    // dice randomness
+}
+
+function gamePlay(){
+    // gameplay will happen here
+    // keeping track of turn score as well as total score
+    // switching turns when the player holds or rolls a 1
+    // the winning condition is the user gets score to 100
+}
