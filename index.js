a808023a9bdaa99a15bfb4efad0d0bd74df7abaa
seanchoii <Vertong05@gmail.com>
2023-10-22 19:23:13 -0600

    created two score bars

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const play = document.getElementById('playButton');
-const game = document.getElementById('game');
+//const play = document.getElementById('playButton');
+//const game = document.getElementById('game');
+//const score = document.getElementById('score');
 
-
-play.addEventListener("click", () => {
+//play.addEventListener("click", () => {
     //document.getElementById('menu').style.display = "none";
     //game.style.display = 'flex';
-})
+    //score.style.display = 'flex';
+//})
+function displayMessage(){
+    // displaying messages like how to play message
+}
 
 function diceAnimation(){
     // dice animation
@@ -18,6 +22,8 @@ function diceRandom(){
 function gamePlay(){
     // gameplay will happen here
     // keeping track of turn score as well as total score
+    // the score bar is modified throught out the game
     // switching turns when the player holds or rolls a 1
     // the winning condition is the user gets score to 100
+    // prints out appropriate messages to the user
 }
